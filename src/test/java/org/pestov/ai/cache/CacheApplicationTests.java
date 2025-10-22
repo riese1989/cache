01041258f57508cd042160a40d2d4ed88858e6ee
Alex Pestov <alexpestov89@yandex.ru>
2025-10-22 16:37:17 +0300

    Первый коммит

---
+++ src/test/java/org/pestov/ai/cache/CacheApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pestov.ai.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
